9b5536d904bbc5718b558eb9fa3bbffff5bc4321
frankomlinovic <fran.komlinovic@gmail.com>
2023-03-26 15:23:12 +0200

    Add navigation

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {Flex, Heading, Image, Menu, MenuItem, withAuthenticator} from '@aws-ampl
 import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from './aws-exports';
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, useNavigate} from "react-router-dom";
 import FieldListView from "./components/fieldListView";
 import FieldDetailWrapper from "./components/fieldDetailWrapper";
 import AppointmentView from "./components/appointmentView";
@@ -15,14 +15,16 @@ import CreateResponseWrapper from "./components/createResponseWrapper";
 Amplify.configure(awsExports);
 
 function App({signOut, user}) {
+    const navigate = useNavigate();
+
     return (
-        <Flex  justifyContent={"flex-start"} direction={"column"}>
+        <Flex justifyContent={"flex-start"} direction={"column"}>
             <Flex direction={"row"} alignItems={"center"} margin={"10px"} justifyContent={"space-between"}>
                 <Image height={"40px"} objectFit={"cover"} src={"korner-logo.png"}/>
                 <Heading level={"3"}>Korner.hr</Heading>
 
                 <Menu marginRight={"10px"}>
-                    <MenuItem id="home" className="menu-item" href="/">Home</MenuItem>
+                    <MenuItem id="home" className="menu-item" onClick={() => navigate("/")}>Home</MenuItem>
                 </Menu>
             </Flex>
 
