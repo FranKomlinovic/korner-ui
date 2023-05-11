a49a4781658c878b1de900043b543c670e044b5d
frankomlinovic <fran.komlinovic@gmail.com>
2023-05-11 11:07:23 +0200

    Add menu to top

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 import {Amplify, Storage} from 'aws-amplify';
 
-import {Button, Flex, Heading, Image, ScrollView, Text, useAuthenticator} from '@aws-amplify/ui-react';
+import {Flex, Heading, Image, Menu, MenuItem, ScrollView, useAuthenticator} from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from './aws-exports';
@@ -10,11 +10,11 @@ import {Route, Routes, useNavigate} from "react-router-dom";
 import Home from "./views/home";
 import FieldById from "./views/fieldById";
 import FieldView from "./views/fieldView";
-import {FaCalendar, FaPlusCircle} from "react-icons/fa";
 import Profile from "./views/profile";
 import {useEffect, useState} from "react";
 import AppointmentById from "./views/appointmentById";
 import {AddToHomeScreen} from "react-pwa-add-to-homescreen";
+import {FaCalendar, FaPlusCircle} from "react-icons/fa";
 
 Amplify.configure(awsExports);
 
@@ -60,42 +60,29 @@ function App() {
     )
 
     const MyMenu = () => (
-        <Flex backgroundColor={"green.90"} direction={"column"}>
-            <Flex marginTop={"0px"} justifyContent={"space-between"}
-                  gap={"0.5rem"}>
-                <Button onClick={() => navigate("/")} border={"none"}>
-                    <Flex alignItems={"center"} gap={"0rem"} direction={"column"}>
-                        <FaCalendar color={"white"} size={"20px"}/><Text fontSize={"small"}
-                                                                         color={"white"}>Termini</Text>
-                    </Flex>
-                </Button>
-                <Button onClick={() => navigate("/fields")} border={"none"}>
-                    <Flex alignItems={"center"} gap={"0rem"} direction={"column"}>
-                        <FaPlusCircle color={"white"} size={"20px"}/><Text fontSize={"small"}
-                                                                           color={"white"}>Rezerviraj</Text>
-                    </Flex>
-                </Button>
-                <Button onClick={() => navigate("/profile")} border={"none"}>
-                    <Flex alignItems={"center"} gap={"0rem"} direction={"column"}>
-                        <Image src={image} onClick={goToProfile} borderRadius={400} objectFit={"cover"} width={"20px"}
-                               height={"20px"}
-                               color={"white"} alt={"Profilna slika"}/>
-                        <Text fontSize={"small"} color={"white"}>Profil</Text>
-                    </Flex>
-                </Button>
-                {/*<Button disabled border={"none"}>*/}
-                {/*    <Flex alignItems={"center"} gap={"0rem"} direction={"column"}>*/}
-                {/*        <FaUsers size={"20px"}/><Text>Prijatelji</Text>*/}
-                {/*    </Flex>*/}
-                {/*</Button>*/}
-            </Flex>
+        <Flex marginBlock={"0.5rem"}>
+            <Menu>
+                <MenuItem onClick={() => navigate("/")}>
+                    <FaCalendar/>Moji termini
+                </MenuItem>
+                <MenuItem onClick={() => navigate("/fields")}>
+                    <FaPlusCircle/>Rezerviraj
+                </MenuItem>
+                <MenuItem onClick={goToProfile}>
+                    <Image src={image} borderRadius={400} objectFit={"cover"} width={"20px"}
+                           height={"20px"}
+                           color={"white"} alt={"Profilna slika"}/>
+                    Profil
+                </MenuItem>
 
+            </Menu>
         </Flex>
     )
 
     const TopHeader = () => (
         <Flex marginInline={"1rem"} justifyContent={"space-between"}>
             <LogoAndAppName/>
+            {user && <MyMenu/>}
         </Flex>
     )
 
@@ -119,7 +106,6 @@ function App() {
             <ScrollView paddingBlock={"1rem"} direction={"column"}>
                 {routes()}
             </ScrollView>
-            {user && <MyMenu/>}
         </Flex>
 
     )
