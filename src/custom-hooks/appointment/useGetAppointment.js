a488f7075854fe7b94ede406a81ab362d18aa770
frankomlinovic <fran.komlinovic@gmail.com>
2023-07-31 20:13:12 +0200

    Appointment load fix?

--- src/custom-hooks/appointment/useGetAppointment.js
+++ src/custom-hooks/appointment/useGetAppointment.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 import {DataStore} from "aws-amplify";
 import {Appointment} from "../../models";
 
@@ -6,19 +6,28 @@ function useGetAppointment(id) {
     const [data, setData] = useState();
     const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const sub = DataStore.observeQuery(Appointment, a => a.id.eq(id)).subscribe(({items}) => {
-            setData(items[0]);
+    const query = useCallback(() => {
+        DataStore.query(Appointment, id).then(a => {
+            setData(a);
             setLoading(false);
+        });
+    }, [id])
+
+    useEffect(() => {
+        query();
+        const subscription = DataStore.observe(Appointment, id).subscribe(() => {
+            query()
         });
 
         return () => {
-            sub.unsubscribe();
-        };
-    }, [id]);
+            subscription.unsubscribe();
+        }
+    }, [id, query]);
+
 
     return {data: data, loading: loading}
 }
+
 
 export default useGetAppointment;
 
