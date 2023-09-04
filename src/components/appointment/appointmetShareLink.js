65b731140718e5cbb5c863acf1769f52da938501
frankomlinovic <fran.komlinovic@gmail.com>
2023-09-04 13:26:59 +0200

    Updated share link

--- src/components/appointment/appointmetShareLink.js
+++ src/components/appointment/appointmetShareLink.js
@@ -1,22 +1,35 @@
-import {Tooltip} from "@mui/material";
-import {Button} from "@aws-amplify/ui-react";
-import {FaLink} from "react-icons/fa";
 import React, {useState} from "react";
 import {getDayAndDateFromAppointment} from "../../functions/converters";
+import {ViberIcon, ViberShareButton, WhatsappIcon, WhatsappShareButton} from "react-share";
+import {FaCopy} from "react-icons/fa";
+import {Button, Flex, Heading} from "@aws-amplify/ui-react";
+import {Tooltip} from "@mui/material";
 
 const AppointmentShareLink = ({appointment, field}) => {
     const [open, setOpen] = useState(false);
 
-    function copyLink() {
-        navigator.clipboard.writeText(
-            "Skupljamo se za termin:\n" + field?.name + "\n" + getDayAndDateFromAppointment(appointment?.date) + "\n" + appointment?.start + " - " + appointment?.end + " \n\nOdgovori na ovoj poveznici:\n" + window.location.href + "\n")
-            .then(() =>  setOpen(true))
+    function copyToClipboard() {
+        navigator.clipboard.writeText(copyLink()).then(() => setOpen(true))
     }
 
-    return !appointment?.locked && <Tooltip onClose={() => setOpen(false)} open={open} leaveTouchDelay={2200}
-                                           title={"Pozivnica kopirana, pošaljite ju prijateljima"}>
-        <Button backgroundColor={"white"} onClick={copyLink}><FaLink/> Pozovi prijatelje</Button>
-    </Tooltip>
+    function copyLink(): string {
+        return "Skupljamo se za termin:\n" + field?.name + "\n" + getDayAndDateFromAppointment(appointment?.date) + "\n" + appointment?.start + " - " + appointment?.end + " \n\nOdgovori na ovoj poveznici:\n" + window.location.href + "\n";
+
+    }
+
+    return !appointment?.locked && <Flex direction={"column"} alignItems={"center"}>
+        <Heading>Pozovi prijatelje</Heading>
+        <Flex>
+            <Tooltip onClose={() => setOpen(false)} open={open} leaveTouchDelay={2200}
+                     title={"Pozivnica kopirana, pošaljite ju prijateljima"}>
+                <Button padding={"0px"} border={"none"} backgroundColor={"transparent"}
+                        onClick={() => copyToClipboard()}><FaCopy size={"2rem"}/></Button>
+            </Tooltip>
+            <WhatsappShareButton url={copyLink()}><WhatsappIcon round size={"2rem"}/> </WhatsappShareButton>
+            <ViberShareButton url={copyLink()}><ViberIcon round size={"2rem"}/> </ViberShareButton>
+        </Flex>
+
+    </Flex>
 }
 
 export default AppointmentShareLink
