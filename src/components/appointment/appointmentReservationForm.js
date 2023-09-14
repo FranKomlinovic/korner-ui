394021b6cc4b797a18e5e654bf07ed71ef1aa4ea
frankomlinovic <fran.komlinovic@gmail.com>
2023-09-14 20:58:27 +0200

    Fixed signout, fixed and refactored appointment guest view

--- src/components/appointment/appointmentReservationForm.js
+++ src/components/appointment/appointmentReservationForm.js
@@ -1,14 +1,17 @@
 import React, {useContext, useEffect, useState} from "react";
-import {Button, Flex, Heading, Text, TextField} from "@aws-amplify/ui-react";
+import {Button, Flex, Heading, TextField} from "@aws-amplify/ui-react";
 import {Response, Team} from "../../models";
 import {DataStore} from "aws-amplify";
-import {FaTshirt} from "react-icons/fa";
 import AlertContext from "../../context/alertContext";
+import LoaderComponent from "../loaderComponent";
+import AppointmentAlreadyAnweredView from "./appointmentAlreadyAnweredView";
 
 const AppointmentReservationForm = ({user, appointment, responses}) => {
     const [name, setName] = useState();
     const [responseToUpdate, setResponseToUpdate] = useState();
     const [team, setTeam] = useState();
+    const [loading, setLoading] = useState(false);
+    const [responded, setResponded] = useState(false);
     const alertContext = useContext(AlertContext);
 
     useEffect(() => {
@@ -28,6 +31,8 @@ const AppointmentReservationForm = ({user, appointment, responses}) => {
 
 
     const createResponse = (accepted) => {
+        setLoading(true)
+        setResponded(true)
         saveResp(accepted);
     };
 
@@ -40,6 +45,7 @@ const AppointmentReservationForm = ({user, appointment, responses}) => {
             playerPhoto: user?.photo,
         });
         DataStore.save(response).then(a => {
+            setLoading(false);
             a.accepted ? alertContext.success("Prihvatili ste termin") :
                 alertContext.warning("Odbili ste termin")
         }).catch(() => {
@@ -47,96 +53,41 @@ const AppointmentReservationForm = ({user, appointment, responses}) => {
         });
     };
 
-    function updateResponse(accepted) {
-        DataStore.save(Response.copyOf(responseToUpdate, (item) => {
-            item.team = undefined;
-            item.accepted = accepted;
-            item.playerPhoto = user?.photo
-        })).then(a => {
-            a.accepted ? alertContext.success("Prihvatili ste termin") :
-                alertContext.warning("Odbili ste termin")
-        }).catch(() => {
-            alertContext.error("Greška prihvaćanja termina, pokušajte ponovno")
-        });
-    }
-
-    const commingButton = (
-        <Button size={"small"} onClick={() => updateResponse(true)} variation={"primary"}>
-            Dolazim
-        </Button>
-    )
-
-    const notComming = (
-        <Button size={"small"} onClick={() => updateResponse(false)} variation={"warning"}>
-            Ne dolazim
-        </Button>
-    )
-
-    const alreadyAnsweredView = () => {
-
-        if (responseToUpdate?.accepted) {
-            if (team) {
-                return (<Flex alignItems={"center"} justifyContent={"center"}>
-
-                    <Heading>Ekipa:</Heading>
-                    <FaTshirt size={"4rem"} color={team?.color}/>
-                    <Heading level={2}>{team?.name}</Heading>
-
-                </Flex>)
-            }
-            return (
-                <Flex direction="column" alignItems={"center"} justifyContent={"center"}>
-                    <Heading color={"green.80"} level={3}>Dolazim</Heading>
-                    {!appointment?.locked &&
-                        <Heading level={6}>Promijeni odgovor:</Heading>}
-                    <Flex>
-                        {!appointment?.locked ? notComming :
-                            <Text textAlign={"center"}>Odgovori su zaključani, za promjenu
-                                kontaktirajte organizatora: {appointment?.bookerName}</Text>}
-                    </Flex>
-                </Flex>
-            );
-        }
-        return (
-            <Flex direction="column" alignItems={"center"} justifyContent={"center"}>
-                <Heading color={"font.warning"} level={3}>Ne dolazim</Heading>
-                {!appointment?.locked && <Heading level={6}>Promijeni odgovor:</Heading>}
-                <Flex>
-                    {!appointment?.locked ? commingButton :
-                        <Text textAlign={"center"}>Odgovori su zaključani, za promjenu
-                            kontaktirajte organizatora: {appointment?.bookerName}</Text>}
-                </Flex>
-            </Flex>
-        );
-
-    }
-
     const createForm = () => {
         if (appointment?.locked) {
             return <Heading textAlign={"center"}>Organizator je ukinuo mogućnost odgovora na termin</Heading>
         }
         return (
-            <Flex direction={"column"}>
-                <TextField
-                    label={"Ime i prezime"}
-                    onChange={(a) => setName(a.currentTarget.value)}
-                    defaultValue={user?.name}
-                />
-                <Flex>
-                    <Button variation={"primary"} onClick={() => createResponse(true)}>
-                        Dolazim
-                    </Button>
-                    <Button onClick={() => createResponse(false)} variation={"warning"}>
-                        Ne Dolazim
-                    </Button>
-                </Flex>
-            </Flex>
+            loading ?
+                <LoaderComponent/> :
+                responded ?
+                    <Flex>
+                        <Heading>Odgovorili ste na termin</Heading>
+                    </Flex>
+                    :
+                    <Flex direction={"column"}>
+                        <TextField
+                            label={"Ime i prezime"}
+                            onChange={(a) => setName(a.currentTarget.value)}
+                            defaultValue={user?.name}
+                        />
+                        <Flex>
+                            <Button variation={"primary"} onClick={() => createResponse(true)}>
+                                Dolazim
+                            </Button>
+                            <Button onClick={() => createResponse(false)} variation={"warning"}>
+                                Ne Dolazim
+                            </Button>
+                        </Flex>
+                    </Flex>
+
         );
     }
 
     return (
         <Flex direction="column" alignItems={"center"} justifyContent={"center"}>
-            {responseToUpdate ? alreadyAnsweredView() : createForm()}
+            {responseToUpdate ? <AppointmentAlreadyAnweredView appointment={appointment} team={team}
+                                                                updateResponseToUpdate={setResponseToUpdate} responseToUpdate={responseToUpdate}/> : createForm()}
         </Flex>
 
 
