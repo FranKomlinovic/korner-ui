07c714d76b58401ebe346b761afe6c8cf90a8619
frankomlinovic <fran.komlinovic@gmail.com>
2023-11-28 14:05:11 +0100

    Fix played appointments

--- src/views/playedAppointments.js
+++ src/views/playedAppointments.js
@@ -7,12 +7,14 @@ import {getCurrentDate} from "../functions/appointmentUItils";
 import {SortDirection} from "@aws-amplify/datastore";
 import FigmaAppointment from "../figma-components/FigmaAppointment";
 import useGetCurrentUser from "../custom-hooks/useGetCurrentUser";
+import LoaderComponent from "../components/loaderComponent";
 
 
 const PlayedAppointments = () => {
     const [playedAppointment, setPlayedAppointment] = useState([]);
     const [responses, setResponses] = useState();
     const [cognitoID, setCognitoID] = useState();
+    const [loading, setLoading] = useState(true);
     const {user} = useGetCurrentUser();
 
     // Set responses
@@ -21,12 +23,16 @@ const PlayedAppointments = () => {
     }, [user]);
     // Set responses
     useEffect(() => {
-        const subscription = DataStore.observeQuery(Response, (c) => c.playerID.eq(cognitoID))
-            .subscribe((resp) => {
-                setResponses(resp.items);
-            });
-
-        return () => subscription.unsubscribe();
+        cognitoID && DataStore.query(Response, b => b.and(
+                c => [
+                    c.accepted.eq(true),
+                    c.playerID.eq(cognitoID)
+                ]), {
+                sort: (sort) => sort.createdAt(SortDirection.DESCENDING)
+            }
+        ).then((resps: []) => {
+            setResponses(resps);
+        });
     }, [cognitoID]);
 
     // Already played appointments
@@ -35,7 +41,7 @@ const PlayedAppointments = () => {
         if (!accepted || accepted?.length === 0) {
             return;
         }
-        DataStore.query(Appointment, b => b.and(
+        accepted && DataStore.query(Appointment, b => b.and(
                 c => [
                     c.or(c => accepted?.map(a => c.id.eq(a))),
                     c.date.lt(getCurrentDate()),
@@ -45,6 +51,7 @@ const PlayedAppointments = () => {
                 sort: (sort) => sort.date(SortDirection.DESCENDING)
             }
         ).then((app) => {
+            setLoading(false);
             setPlayedAppointment(app);
         });
 
@@ -52,15 +59,16 @@ const PlayedAppointments = () => {
 
 
     return (
-        <Flex alignItems={"center"} direction={"column"} marginTop={"1rem"}>
-            <Heading level={4} variation={"primary"}>Odigrani termini: {playedAppointment.length}</Heading>
-            {playedAppointment.length === 0 ? <Heading level={6}>Nemate odigranih termina</Heading> :
-                playedAppointment.map(a => {
-                    return <Flex key={a.id}>
-                        <FigmaAppointment appointment={a}/>
-                    </Flex>;
-                })}
-        </Flex>
+        loading ? <LoaderComponent/> :
+            <Flex alignItems={"center"} direction={"column"} marginTop={"1rem"}>
+                <Heading level={4} variation={"primary"}>Odigrani termini: {playedAppointment.length}</Heading>
+                {playedAppointment.length === 0 ? <Heading level={6}>Nemate odigranih termina</Heading> :
+                    playedAppointment.map(a => {
+                        return <Flex key={a.id}>
+                            <FigmaAppointment appointment={a}/>
+                        </Flex>;
+                    })}
+            </Flex>
 
     );
 
